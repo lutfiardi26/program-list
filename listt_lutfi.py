aa3875a4847d1357b1769fc349505370052bae9f
lutfiardi26 <117370339+lutfiardi26@users.noreply.github.com>
2022-11-13 20:51:34 +0700

    Add files via upload

---
+++ listt_lutfi.py
@@ -0,0 +1,9 @@
+list1 = ['Lutfi','Ardiansyah']
+list2 = ['Serang','Cikande','Kemayoran']
+print(list1[0])
+print(list2[0])
+
+list1.append('Lutfi_Ardiansyah')
+list2.remove('Serang')
+print (list1[2])
+print (list2[0])
